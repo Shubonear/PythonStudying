198945969c5e72a6e55cdbcd1724f769f5439e71
Student <student@lan.itstep.kiev.ua>
2021-07-01 22:08:07 +0300

    Python

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ operation = input("Введите операцию: ")
 number_2 = input("Введите второе число: ")
 is_digit2 = number_2.isdigit()
 if is_digit and is_digit2:
+    number = float(number)
+    number_2 = float(number_2)
     if operation == "-":
-        print("=", float(number)-float(number_2))
+        print("=", number-number_2)
     elif operation == "+":
-        print("=", float(number)+float(number_2))
+        print("=", number+number_2)
     elif operation == "*":
-        print("=", float(number)*float(number_2))
+        print("=", number*number_2)
     elif operation == "/" or "|":
-        print ("=", float(number)/float(number_2))
+        print ("=", number/number_2)
 else:
         print("Пожалуйста, введите числа цифрами.")
-
-
